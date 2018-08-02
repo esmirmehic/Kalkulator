6cf82538d89fe33d30c952419e5ee17fe7d17d39
Mehic Esmir <esmir.mehiccc@gmail.com>
2018-08-02 21:20:17 +0200

    ""

---
+++ Kalkulator.java
@@ -0,0 +1,21 @@
+
+public class Kalkulator {
+void sabiranje () {
+	
+}
+void oduzimanje () {
+	
+}
+void mnozenje () {
+	
+}
+void dijeljenje () {
+	
+}
+void korjenovanje () {
+	
+}
+void kvadriranje () {
+	
+}
+}
